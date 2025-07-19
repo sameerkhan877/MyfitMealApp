f540791d1a2e322918c53557c5916878fa85d26a
sameer <sameer@SAMEER>
2025-07-19 19:36:09 +0530

    Add project files.

---
+++ MyfitMealApp/tailwind.config.js
@@ -0,0 +1,32 @@
+﻿///** @type {import('tailwindcss').Config} */
+//module.exports = {
+//    content: [
+//        './Views/**/*.cshtml',
+//        './wwwroot/js/**/*.js'    ],
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [],
+//}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Views/**/*.cshtml",
+        "./wwwroot/**/*.html",
+        "./**/*.cshtml"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                greenish: "#518023",
+                red: "#ff4d4d",
+            },
+            backgroundImage: {
+                'btn-border-gradient': 'linear-gradient(90deg, #e8cbc0, #636fa4)',
+            }
+        },
+    },
+    plugins: [],
+    important: true,
+}
---
+++ MyfitMealApp/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿  const cursor = document.getElementById("cursor");
+
+  document.addEventListener("mousemove", (e) => {
+    // Only apply on screens ≥ 1024px
+    if (window.innerWidth >= 1024) {
+      const cursorSize = 24; // if w-6 h-6 (6 x 4 = 24px)
+      const offset = cursorSize / 2;
+
+      gsap.to(cursor, {
+        x: e.clientX - offset,
+        y: e.clientY - offset,
+        duration: 0.15,
+        ease: "power2.out"
+      });
+    }
+  });
+
+
+// Scale up on hover
+document.querySelectorAll('h1, button, a').forEach(elem => {
+    elem.addEventListener("mouseenter", () => {
+        gsap.to(cursor, { scale: 2, duration: 0.3 });
+    });
+    elem.addEventListener("mouseleave", () => {
+        gsap.to(cursor, { scale: 1, duration: 0.3 });
+    });
+});
